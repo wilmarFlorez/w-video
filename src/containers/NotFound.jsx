8d0c440ab638ac6b8742fe97b425b20e91c36dd4
wilmar52 <wflorez52@gmail.com>
2019-11-25 17:54:25 -0500

    Added react router and redux

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const NotFound = () =>
+  <React.Fragment>
+    <h1>No hemos podido encontrar lo que estas buscando ;(</h1>
+  </React.Fragment>
+
+export default NotFound
